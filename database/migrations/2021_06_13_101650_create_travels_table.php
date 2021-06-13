727b9850fa1dd683d9e8962ccd6284b284b11d16
Michele Lorusso <kirito102095@hotmail.it>
2021-06-13 14:21:22 +0200

    Complete

---
+++ database/migrations/2021_06_13_101650_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string("package_name", 25);
+            $table->string("city", 20);
+            $table->text("description");
+            $table->tinyInteger("days_number");
+            $table->date("start_date");
+            $table->date("end_date");
+            $table->float("price", 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,47 @@
-<h1>ciao</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Trivago</title>
+
+</head>
+<body>
+
+    <div>
+        <img style="width: 200px;"src="{{ asset('img/1280px-Trivago.svg.png')}}" alt="trivago">
+    </div>
+
+    <h2>Pacchetti Giugno/Luglio</h2>
+
+
+    <ul>
+            @foreach ($travels as $travel)
+                <li>
+
+                    <h2 style="text-transform: capitalize">{{$travel->package_name}}</h2>
+
+                    <h3>{{$travel->city}}</h3>
+
+                    <div>{{$travel->description}}</div>
+
+                    <ul>
+
+                        <li>
+                            <div>{{$travel->days_number}} giorni</div>
+                            <div>Dal: {{$travel->start_date}} - Al: {{$travel->end_date}}</div>
+                        </li>
+
+                    </ul>
+                    
+                    <div>Price: {{$travel->price}}</div>
+
+                </li>
+                
+            @endforeach
+        </ul>
+    
+</body>
+</html>
